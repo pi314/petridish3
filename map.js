a4dc9672fcbd571948ba9fe261612e6a02cd20c0
Cychih <michael66230@gmail.com>
2015-07-13 22:06:31 +0800

    initial commit, petridish DOM

---
+++ map.js
@@ -0,0 +1,17 @@
+var map = {};
+
+map.WIDTH = 27;
+map.HEIGHT = 27;
+
+map.init = function () {
+    var s = '';
+    for (var i = 0; i < map.HEIGHT; i++) {
+        s += '<div class="row">';
+        for (var j = 0; j < map.WIDTH; j++) {
+            s += '<div id="cell-'+ i +'-'+ j +'" class="b-empty"></div>';
+        }
+        s += '</div>';
+    }
+    $('#petridish').append(s);
+
+};
