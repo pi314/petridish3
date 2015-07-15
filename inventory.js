a2f35663358ca9306a2a0115af7f1808125752ed
Cychih <michael66230@gmail.com>
2015-07-15 22:14:03 +0800

    refine inventory.add function

--- inventory.js
+++ inventory.js
@@ -1,6 +1,10 @@
 var inventory = {};
 
-inventory.add = function (cell_gene) {
+inventory.add = function (cell_gene, amount) {
+    if (amount == undefined) {
+        amount = 1;
+    }
+
     if (inventory[cell_gene] == undefined) {
         inventory[cell_gene] = 0;
         var d = $('<div id="'+ cell_gene +'"class="inventory-cell-item">');
@@ -10,6 +14,6 @@ inventory.add = function (cell_gene) {
         d.append(n);
         $('#inventory-cells').append(d);
     }
-    inventory[cell_gene] += 1;
+    inventory[cell_gene] += amount;
     $('#'+ cell_gene +' > .cell-amount').text(inventory[cell_gene]);
 };
