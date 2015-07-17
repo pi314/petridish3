1a3a20a20d574b4a485ebeb56052bc4fa3a036d3
Cychih <michael66230@gmail.com>
2015-07-17 23:32:07 +0800

    randomized cell growing

--- cell-group.js
+++ cell-group.js
@@ -14,10 +14,11 @@ function cell_group (r, g, b) {
     this.col = null;
 
     // member cell list
-    this.member = [];
+    this.member = {};
 
     this.growth_counter = 0;
     this.grow_flag = false;
+    this.grow_distance = null;
 }
 
 cell_group.prototype.gene = function () {
@@ -73,6 +74,7 @@ cell_group.prototype.generate_pulse = function () {
 
     if (t.growth_counter >= t.growth_delay) {
         t.grow_flag = true;
+        t.grow_distance = sample(Object.keys(t.member).filter(function (x) { return x != 'Infinity'}));
         t.growth_counter = 0;
     }
 
@@ -91,21 +93,30 @@ cell_group.prototype.wave_up = function (wave_distance) {
         wave_distance = 0;
     }
 
+    var available_space = [];
     for (var i in this.member[wave_distance]) {
-        var c = t.member[wave_distance][i];
-        c.dom.removeClass('block').addClass('pulse-block');
-        var v = new vector(c.row, c.col);
+        var this_c = t.member[wave_distance][i];
+        this_c.dom.removeClass('block').addClass('pulse-block');
+        var this_coord = new vector(this_c.row, this_c.col);
         for (var j = 0; j < SHAPE_VECTOR[this.shape].length; j++) {
-            var vv = v.add(SHAPE_VECTOR[this.shape][j]);
-            var cc = map.get_cell_at(vv)
-            if (cc == null) {
-                if (t.grow_flag) {
-                    t.put_cell(vv);
-                    t.grow_flag = false;
+            var neighbor_coord = this_coord.add(SHAPE_VECTOR[this.shape][j]);
+            var neighbor_cell = map.get_cell_at(neighbor_coord)
+            if (neighbor_cell == null) {
+                if (wave_distance == t.grow_distance && t.grow_flag) {
+                    available_space.push(neighbor_coord);
                 }
-            } else if (cc.distance == Infinity || c.distance + 1 < cc.distance) {
-                cc.set_distance(c.distance + 1);
+            } else if (neighbor_cell.distance == Infinity || this_c.distance + 1 < neighbor_cell.distance) {
+                neighbor_cell.set_distance(this_c.distance + 1);
             }
+        }
+    }
+
+    if (wave_distance == t.grow_distance) {
+        var neighbor_coord = sample(available_space)
+        if (neighbor_coord != undefined) {
+            t.put_cell(neighbor_coord);
+        } else {
+            t.grow_distance += 1;
         }
     }
 
@@ -118,86 +129,6 @@ cell_group.prototype.wave_up = function (wave_distance) {
 cell_group.prototype.wave_down = function (wave_distance) {
     for (var i in this.member[wave_distance]) {
         this.member[wave_distance][i].dom.removeClass('pulse-block').addClass('block');
-    }
-};
-
-cell_group.prototype.grow_cell = function () {
-    var t = this;
-    if (t.row == null) { return; }
-    var new_cell_direction = sample([U, L, R, D].filter(function (x) {
-        return t.distance_map[x.row][x.col] > t.distance_map[0][0];
-    }));
-    var new_cell_coord = new_cell_direction.add(new vector(t.row, t.col));
-    var target_cell = map.get_cell_at(new_cell_coord);
-    if (target_cell == null) {
-        map.put_cell(t.copy(), new_cell_coord);
-    } else if (target_cell.gene() == t.gene()) {
-        var msg = {};
-        msg.type = MSG_GROW;
-        msg.master = this.dom.attr('id');
-        msg.distance = 0;
-        t.send(msg, new_cell_direction)
-    }
-};
-
-cell_group.prototype.send = function (msg, direction) {
-    if (this.row == null) { return; }
-    var v = new vector(this.row, this.col).add(direction);
-    var other_cell = map.get_cell_at(v);
-    if (other_cell == null) { return; }
-    $(other_cell).trigger('MESSAGE', [msg, direction.reverse]);
-};
-
-cell_group.prototype.receive = function (e, msg, from) {
-    var t = this;
-    switch (msg.type) {
-    case MSG_WAVE:
-        t.pulse_queue[from.row][from.col] = msg;
-        t.distance_map[from.row][from.col] = msg.distance;
-        if (msg.distance + 1 < t.distance_map[0][0]) {
-            t.distance_map[0][0] = msg.distance + 1;
-        }
-        switch (t.master) {
-        case null:
-            t.master = msg.master;
-            break;
-        case msg.master:
-            break;
-        default:
-            break;
-        }
-        t.pulse_queue_dirty = true;
-        if (t.dom) {
-            t.dom.removeClass('block').addClass('pulse-block');
-            // t.dom.text(t.distance_map[0][0]);
-        }
-
-        if (t.pulse_queue_dirty) {
-            setTimeout(function () {
-                var msg = {};
-                msg.type = MSG_WAVE;
-                msg.distance = t.distance_map[0][0];
-                msg.master = t.master;
-                if (t.dom) {
-                    t.dom.removeClass('pulse-block').addClass('block');
-                }
-                for (var i = 0; i < SHAPE_VECTOR[t.shape].length; i++) {
-                    var sv = SHAPE_VECTOR[t.shape][i]
-                    if (t.distance_map[sv.row][sv.col] > t.distance_map[0][0]) {
-                        t.send(msg, SHAPE_VECTOR[t.shape][i]);
-                    }
-                }
-                t.pulse_queue_dirty = false;
-                if (t.growth_flag) {
-                    t.grow_cell();
-                    t.growth_flag = false;
-                }
-            }, t.pulse_delay * PULSE_DELAY_UNIT);
-        }
-        break;
-    case MSG_GROW:
-        t.growth_flag = true;
-        break;
     }
 };
 
