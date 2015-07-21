c0012be384051aea10dd1bc5f4d9e80301cadfdc
Cychih <michael66230@gmail.com>
2015-07-21 16:10:08 +0800

    rename css .row to .map-row

--- map.js
+++ map.js
@@ -9,7 +9,7 @@ map.HEIGHT = 27;
 map.init = function () {
     var s = '';
     for (var i = 0; i < map.HEIGHT; i++) {
-        s += '<div class="row">';
+        s += '<div class="map-row">';
         for (var j = 0; j < map.WIDTH; j++) {
             s += '<div id="cell-{}-{}" class="block empty"></div>'.format(i, j);
         }
